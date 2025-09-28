8e876b3d685e3a55968355c4f3594b24edbf0ab3
rahulm-04 <raahulix01@gmail.com>
2025-09-28 16:33:39 +0530

    cha

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -10,7 +10,7 @@ export default function Hero() {
       {/* Background Image */}
       <div className="absolute inset-0">
         <img
-          src="/ewf.jpg"
+          src="./ewf.jpg"
           alt="background"
           className="w-full h-full object-cover"
         />
