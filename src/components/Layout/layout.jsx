08bee759f6b2cfe2b6bd764f56b6bea2967769db
rahulm-04 <raahulix01@gmail.com>
2025-09-28 16:55:16 +0530

    changes

--- src/components/Layout/layout.jsx
+++ src/components/Layout/layout.jsx
@@ -12,7 +12,7 @@ const VideoLayout = ({ children }) => {
         playsInline
         className="fixed inset-0 w-full h-full object-cover -z-10"
       >
-        <source src="./stt.mp4" type="video/mp4" />
+        <source src="/portfolio/stt.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
